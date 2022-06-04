bcb5c3763493efdd7355a927e5b28a23120fd4af
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-06-03 17:08:50 -0700

    save updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,32 @@
 import './App.css';
 
 function App() {
+
+ // function initMap() {
+
+    const uluru = { lat: -25.344, lng: 131.031 };
+  
+    const map = new google.maps.Map({
+        zoom: 10,
+        center: uluru,
+    });
+   
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+  //}
+  
+  //window.initMap = initMap;
+
+
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <p>Input number of available drivers:  </p>
+    <input type="number"></input>
+    <div id="map">{map}</div>
        
       </header>
     </div>
